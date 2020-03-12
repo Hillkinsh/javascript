43bd4f7ba0d1284bc088b775e8a83d8a98210ac0
xuqingshan <xuqingshan@vipkid.com.cn>
2020-03-12 15:27:38 +0800

    重整目录

---
+++ js语言精粹/数据类型.js
@@ -0,0 +1,10 @@
+// 1.常用的数据类型
+
+// string, boolearn, number, null, undefined, symbol
+// object, array, function
+
+// 2. 数据类型检测
+
+// 1. typeof null会被返回object
+// 2. instance 只能判断实例关系，不能判断数据类型
+// 3. Object.prototype.toString方法
--- webpack.js
+++
@@ -1,9 +0,0 @@
-// clean-webpack-plugin 清理构建目录
-
-// postcss autoprefixer
-
-// 基础库分离：html-webpackexternals-plugin
-// SplitChunksPlugin
-
-// 一个webpack过程
-webpack
