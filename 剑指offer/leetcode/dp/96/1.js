fc5e7a48a1fcdf41f5abf83c2d61c2ce7a223c82
xuqingshan <xuqingshan@vipkid.com.cn>
2020-03-12 08:54:19 +0800

    优化方法

--- 剑指offer/leetcode/dp/96/1.js
+++ 剑指offer/leetcode/dp/96/1.js
@@ -1,22 +1,25 @@
 /**
- * @param {string} s
+ * @param {number} n
  * @return {number}
  */
-var numDecodings = function (s) {
-  if (!s || s.length == 1 && s[0] == '0') return 0
-  let dp = [1]
-  for (let i = 0; i < s.length; i++) {
-    /**
-     * 当前数字如果为0，那么只能dp[i+1] = dp[i-1] // 一种情况
-     * 当前数字不为0，dp[i+1] = dp[i] 如果本身还能拼，则dp[i+1] = dp[i] + dp[i-1]
-     */
-    dp[i + 1] = s[i] == '0' ? 0 : dp[i]
-    if (i > 0 && (s[i] == 1 || s[i - 1] == 2 && s[i] - 6 <= 0)) {
-      dp[i + 1] += dp[i - 1]
+var numTrees = function (n) {
+  let dp = [1, 1]
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      if (!dp[i]) dp[i] = 0
+      dp[i] += dp[j - 1] * dp[i - j]
+      // g(n) = 
+      // console.log(dp)
     }
   }
-  return dp[dp.length - 1]
-  // console.log(dp)
+  return dp[n]
 };
-
-console.log(numDecodings('226'))
+/**
+ * 思路：关键是这个推导式：
+ * 1），G(n) = f(1) + f(2) + ... + f(n)
+ * // fn 是以n为跟节点的树个数
+ * f(n) 其左子树节点个数为[1,2,3,...,i-1]，右子树节点个数为[i+1,i+2,...n]
+ * f(n) = g(0)*g(n-1)  + g(1)*g(n-2) + ... + g(n-1)* g(0)
+ */
+// console.log(numTrees(3))
+// console.log(numTrees(2))
