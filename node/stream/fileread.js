86de295268be110f494ba03372cabc9b44b6f43b
xuqingshan <xuqingshan@vipkid.com.cn>
2020-04-06 14:59:10 +0800

    rm stream

--- node/stream/fileread.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const server = require('http').createServer()
-
-server.on('request', (req, res) => {
-  let src = fs.createReadStream('./bigFile')
-    src.pipe(res)
-})
-
-server.listen(3000, _ => console.log('server is running.'))
