df113f72627de32d3b6b034a63753a029406509e
xuqingshan <xuqingshan@vipkid.com.cn>
2020-04-15 10:13:07 +0800

    ts

--- js语言精粹/proxy.js
+++
@@ -1,9 +0,0 @@
-let hasProxy = new Proxy({}, {
-  has(target, proxyKey) {
-    console.log('has option')
-    return proxyKey in target
-  }
-})
-
-hasProxy['hello'] = 'world'
-console.log('hell' in hasProxy)
